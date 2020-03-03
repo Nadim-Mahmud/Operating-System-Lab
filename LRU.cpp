baca96cf66dfb911ec494f3687407030521e4cbb
Nadim-Mahmud <mahmudnadim16@gmail.com>
2020-03-04 00:01:59 +0600

    page fault added

--- LRU.cpp
+++ LRU.cpp
@@ -1,47 +1,44 @@
-/// Incomplete
-
-
 #include<bits/stdc++.h>
 #define pii pair<int,int>
 using namespace std;
 #define MX 10000000
 
-int ara[MX],mr[MX],frame_sz;
-pii st[MX];
+int ara[MX],frame_sz;
+int frame[MX];
+int timec[MX+MX+MX];
+int mp[MX];
+
+int putPosition(){
+    int in=0;
+    for(int i=0;i<frame_sz;i++){
+        if(frame[i]==-1) return i;
+        if(timec[frame[i]]<timec[frame[in]]){
+            in = i;
+        }
+    }
+    return in;
+}
 
 void LRU(int n){
     int cn=0,i,in=0,j,mn,mni;
-    memset(mr,0,sizeof mr);
-    for(i=1;i<=n;i++){
-        if(!mr[ara[i]]){
+
+    memset(mp,0,sizeof mp);
+    memset(timec,0,sizeof timec);
+    memset(frame,-1,sizeof frame);
+
+    for(i=0;i<n;i++){
+        if(!mp[ara[i]]){
             cn++;
-            //cout<<ara[i]<<endl;
-            //for(j=1;j<=frame_sz;j++) cout<<st[j].first<<" ";
-            //puts("");
-        }
-        if(in<frame_sz){
-            st[++in].first = ara[i];
-            st[in].second = i;
-        }
-        else if(!mr[ara[i]]){
-            mn = st[1].second;
-            mni = 1;
-            for(j=1;j<=frame_sz;j++){
-                if(st[j].second<mn){
-                    mn = st[j].second;
-                    mni = j;
-                }
+            in = putPosition();
+            if(frame[in]!=-1){
+                mp[frame[in]] = 0;
             }
-            mr[st[mni].first] = 0;
-            st[mni].first = ara[i];
-            st[mni].second = i;
+            frame[in] = ara[i];
+            mp[ara[i]] = 1;
         }
-        else{
-            st[mr[ara[i]]].second = i;
-        }
-        mr[ara[i]] = i;
+        timec[ara[i]] = i;
     }
-    puts("LRU Fault : ");
+    cout<<"LRU page fault : ";
     cout<<cn<<endl;
 }
 
@@ -49,7 +46,7 @@ int main(){
     int i,j,k,l,n;
     freopen("lru.txt","r",stdin);
     cin>>n>>frame_sz;
-    for(i=1;i<=n;i++){
+    for(i=0;i<n;i++){
         cin>>ara[i];
     }
     LRU(n);
